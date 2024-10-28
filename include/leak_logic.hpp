1bec84c38a4f81da7e68b6515445373a444c3021
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-10-28 23:50:20 +0100

    [#1] Add basic leak logic implementation

--- include/leak_logic.hpp
+++ include/leak_logic.hpp
@@ -1,40 +1,156 @@
 #ifndef LEAK_LOGIC_HPP
 #define LEAK_LOGIC_HPP
 
-#include <iostream>
+#include "leakguard/staticvector.hpp"
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+
+#define LEAK_LOGIC_MAX_CRITERIA 10
 
 namespace lg {
 
-struct LeakLogicState {
+    enum class ActionType {
+        NO_ACTION,
+        CLOSE_VALVE
+    };
 
-};
+    enum class ActionReason {
+        NONE,
+        EXCEEDED_FLOW_RATE,
+        LEAK_DETECTED_BY_PROBE
+    };
 
-/**
- * @brief Leak detection logic.
- */
-class LeakLogic {
-public:
+    struct SensorState {
+        float flowRate;
+        StaticVector<bool, 256>& probeStates;
+    };
+
+    class LeakPreventionAction {
+    public:
+        explicit LeakPreventionAction(
+            const ActionType actionType = ActionType::NO_ACTION,
+            const ActionReason reason = ActionReason::NONE,
+            const uint8_t probeId = -1)
+                : actionType(actionType), reason(reason), probeId(probeId) {}
+
+        [[nodiscard]] ActionType getActionType() const { return actionType; }
+        [[nodiscard]] ActionReason getActionReason() const { return reason; }
+        [[nodiscard]] uint8_t getProbeId() const { return probeId; }
+
+    private:
+        ActionType actionType;
+        ActionReason reason;
+        uint8_t probeId;
+    };
+
+    class LeakDetectionCriterion {
+    public:
+        virtual void update(const SensorState& sensorState, time_t elapsedTime) = 0;
+
+        [[nodiscard]] virtual std::optional<LeakPreventionAction> getAction() const = 0;
+
+        virtual ~LeakDetectionCriterion() = default;
+    };
+
+    class TimeBasedFlowRateCriterion final : public LeakDetectionCriterion {
+    public:
+        TimeBasedFlowRateCriterion(const float rateThreshold, const time_t minDuration)
+            : rateThreshold(rateThreshold), minDuration(minDuration),
+              accumulatedTime(0), active(false) {}
+
+        void update(const SensorState& sensorState, const time_t elapsedTime) override {
+            if (sensorState.flowRate >= rateThreshold) {
+                accumulatedTime += elapsedTime;
+                active = true;
+            }
+            else {
+                accumulatedTime = 0;
+                active = false;
+            }
+        }
+
+        [[nodiscard]] std::optional<LeakPreventionAction> getAction() const override {
+            if (active && accumulatedTime >= minDuration) {
+                return LeakPreventionAction(
+                    ActionType::CLOSE_VALVE,
+                    ActionReason::EXCEEDED_FLOW_RATE
+                );
+            }
+            return std::nullopt;
+        }
+
+    private:
+        float rateThreshold;
+        time_t minDuration;
+        time_t accumulatedTime;
+
+        bool active;
+    };
+
+    // Maybe we shouldn't have this? This is done for clarity, but it'd require separate criterion objects for each probe
+    class ProbeLeakDetectionCriterion final : public LeakDetectionCriterion {
+    public:
+        explicit ProbeLeakDetectionCriterion(const uint8_t probeId) : probeId(probeId) {}
+
+        void update(const SensorState& sensorState, const time_t elapsedTime) override {
+            leakDetected = false;
+            for (const auto state : sensorState.probeStates) {
+                if (state) {
+                    leakDetected = true;
+                }
+            }
+        }
+
+        [[nodiscard]] std::optional<LeakPreventionAction> getAction() const override {
+            if (leakDetected) {
+                return LeakPreventionAction(
+                    ActionType::CLOSE_VALVE,
+                    ActionReason::LEAK_DETECTED_BY_PROBE,
+                    probeId
+                );
+            }
+            return std::nullopt;
+        }
+
+    private:
+        uint8_t probeId;
+        bool leakDetected = false;
+    };
+
     /**
-     * @brief Updates the logic with the current state of the sensors and probes.
-     *
-     * @param state A struct describing the sensors' and probes' state.
+     * @brief Leak detection logic.
      */
-    void updateState(LeakLogicState&& state) {
-        currentState = state;
-    }
+    class LeakLogic {
+    public:
+        static LeakLogic& getInstance() {
+            static LeakLogic instance;
+            return instance;
+        }
 
-    /**
-     * @brief Ticks the logic, evaluating all set schedules and determining whether a leak occurred.
-     *
-     * @param currentTime The current timestamp. TODO: pick a unit for this - Unix timestamp might be good
-     */
-    void tick(time_t currentTime) {
-        // TODO
-    }
+        void update(const SensorState& sensorState, const time_t elapsedTime) {
+            for (const auto& criterion : criteria) {
+                criterion->update(sensorState, elapsedTime);
+            }
+        }
 
-private:
-    LeakLogicState currentState;
-};
+        [[nodiscard]] LeakPreventionAction getAction() const {
+            for (auto& criterion : criteria) {
+                if (const auto action = criterion->getAction()) {
+                    return *action;
+                }
+            }
+            return LeakPreventionAction(ActionType::NO_ACTION);
+        }
+
+        void addCriterion(std::shared_ptr<LeakDetectionCriterion> criterion) {
+            criteria.Append(std::move(criterion));
+        }
+
+    private:
+        StaticVector<std::shared_ptr<LeakDetectionCriterion>, LEAK_LOGIC_MAX_CRITERIA> criteria;
+    };
 
 
 }
--- test/test_driver.cpp
+++ test/test_driver.cpp
@@ -1,9 +1,39 @@
 #include "leak_logic.hpp"
 #include <gtest/gtest.h>
 
-TEST(LeakLogicTest, BaseTest)
-{
+TEST(LeakLogicTest, ShouldDetectLeakWithFlowMeter) {
+    lg::LeakLogic logic = lg::LeakLogic::getInstance();
+    lg::StaticVector<bool, 256> probeStates;
 
+    // Detect leak if flow rate exceeds 2 L/min for at least 1 minute
+    logic.addCriterion(std::make_unique<lg::TimeBasedFlowRateCriterion>(2.0f, 60));
+
+    // Flow rate of 3 L/min for 30 secs
+    logic.update(lg::SensorState(3, probeStates), 30);
+    ASSERT_EQ(logic.getAction().getActionType(), lg::ActionType::NO_ACTION);
+
+    // Continue flow for 30 secs
+    logic.update(lg::SensorState(3, probeStates), 30);
+    ASSERT_EQ(logic.getAction().getActionType(), lg::ActionType::CLOSE_VALVE);
+}
+
+TEST(LeakLogicTest, ShouldNotDetectLeakIfFlowStops) {
+    lg::LeakLogic logic = lg::LeakLogic::getInstance();
+    lg::StaticVector<bool, 256> probeStates;
+
+    // Detect leak if flow rate exceeds 2 L/min for at least 1 minute
+    logic.addCriterion(std::make_unique<lg::TimeBasedFlowRateCriterion>(2.0f, 60));
+
+    // Flow rate of 3 L/min for 30 secs
+    logic.update(lg::SensorState(3, probeStates), 30);
+    ASSERT_EQ(logic.getAction().getActionType(), lg::ActionType::NO_ACTION);
+
+    // Cut flow immediately
+    logic.update(lg::SensorState(0, probeStates), 0);
+
+    // Wait 30 seconds
+    logic.update(lg::SensorState(0, probeStates), 30);
+    ASSERT_EQ(logic.getAction().getActionType(), lg::ActionType::NO_ACTION);
 }
 
 int main(int argc, char **argv)
